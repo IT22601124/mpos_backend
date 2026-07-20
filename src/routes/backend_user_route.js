596e7d7e82a120e9d4afd4bfa6261023bc8e5260
Tharindu <it22601124@my.sliit.lk>
2026-07-20 19:35:39 +0530

    ass

--- src/routes/backend_user_route.js
+++ src/routes/backend_user_route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const backendUserController = require('../controllers/backend_user_controller');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/auth', backendUserController.createBackendUser);
+router.post('/auth/users', backendUserController.createBackendUser);
 router.post('/auth/login', backendUserController.loginBackendUser);
 router.get('/auth/users',authMiddleware, backendUserController.getAllBackendUsers);
 router.post('/auth/verify-token', authMiddleware, backendUserController.verifyToken);
